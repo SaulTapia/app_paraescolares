46a95e3a4058d31a3b6a5854baca35a80cdea521
SaulTapia <19080045@cobachih.edu.mx>
2022-07-24 12:28:12 -0600

    Add gunicorn, set SECRET_KEY

--- app_paraescolares/settings.py
+++ app_paraescolares/settings.py
@@ -11,7 +11,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pkq5f$-7ym=rik+cr!v62p-ukk2!cd^g#i%-h1d&m^f(1rz646'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
