93726a1880feb0abcef584e4f8ce952d662bc108
SaulTapia <19080045@cobachih.edu.mx>
2022-08-08 19:08:28 -0600

    Add whitenoise

--- app_paraescolares/settings.py
+++ app_paraescolares/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "corsheaders.middleware.CorsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 
