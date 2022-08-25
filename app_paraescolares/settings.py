bf61766fdab5859378b89c13675790cd6f33330f
SaulTapia <19080045@cobachih.edu.mx>
2022-08-25 17:27:02 -0600

    Adjust allowed host

--- app_paraescolares/settings.py
+++ app_paraescolares/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 
 ALLOWED_HOSTS = [
-    'app-paraescolares.herokuapp.com',
+    'app-paraescolares-production.herokuapp.com',
     '127.0.0.1',
 ]
 
